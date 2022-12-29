15e6d118b0f14b01a27e0b5cad12f0d6187817f4
zzzMunchizzz <jmllerenaespinoza@gmail.com>
2022-12-29 17:32:50 -0500

    Sesion 2 v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PruebaComponent } from './prueba/prueba.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PruebaComponent
   ],
   imports: [
     BrowserModule
